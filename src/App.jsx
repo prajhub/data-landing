67b46f6b5427802d8769f850698b58cd03874041
prajhub <pzlmagar@gmail.com>
2023-06-05 12:32:59 +0545

    Added animate on scrol.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import Analytics from './components/Analytics'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  Aos.init({
+    duration: 1800,
+    offset: 0,
+  })
+
+ 
 
   return (
-    <>
-     <div>hi</div>
-    </>
-  )
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  );
 }
 
 export default App
