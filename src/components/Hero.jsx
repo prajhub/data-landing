8edb0dd1130b15252050026a6ad2e6182ff63a25
prajhub <pzlmagar@gmail.com>
2023-06-05 16:16:17 +0545

    Removed react-typed.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Typed from 'react-typed';
+
 
 const Hero = () => {
   return (
@@ -12,16 +12,10 @@ const Hero = () => {
           Grow with data.
         </h1>
         <div className='flex justify-center items-center'>
-          <p  className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
+          <p data-aos='fade-down' data-aos-delay='500'  className='md:text-5xl tracking-3 sm:text-4xl text-xl font-bold py-4'>
+            Fast, flexible financing for BTB, BTC & SASS
           </p>
-          <Typed 
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['BTB', 'BTC', 'SASS']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
+         
         </div>
         <p data-aos='fade-down' data-aos-delay='600' className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
         <button data-aos='fade-up' data-aos-delay='700' className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
